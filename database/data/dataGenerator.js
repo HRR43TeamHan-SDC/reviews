6a646a61c63745355922e2d709cdba91b2bbb97e
JohnCharamonde <john.charamonde@gmail.com>
2020-02-15 16:39:09 -0500

    Syntactic updates to make seed run correctly

--- database/data/dataGenerator.js
+++ database/data/dataGenerator.js
@@ -15,11 +15,11 @@ const jabber = new Jabber(buzzWords, 2);
 const variables = {
   numRestaurants: 30000,
   totalNumRecordsToWrite: 10000000,
-  recordsToGenerateEachTime: 5000,
+  recordsToGenerateEachTime: 10000,
 }
 
 const csvWriter = createCsvWriter({
-  path: './data.csv',
+  path: './data3.csv',
   header: [
       {id: 'restaurantId', title: 'restaurantId'},
       {id: 'firstName', title: 'firstName'},
@@ -42,7 +42,7 @@ const csvWriter = createCsvWriter({
 
 let generateRecords = () => {
   let records = [];
-  if(records.length < variables.recordsToGenerateEachTime + 1) {
+  while (records.length < variables.recordsToGenerateEachTime) {
     records.push({
       restaurantId: Math.floor(Math.random() * variables.numRestaurants),
       firstName: faker.name.firstName(),
@@ -60,11 +60,12 @@ let generateRecords = () => {
       filterTag: tags[Math.floor(Math.random() * tags.length)],
       vip: Math.random() < 0.3,
       color: circleColors[Math.floor(Math.random() * circleColors.length)],
-    })
-    generateRecords();
+    });
   }
   return records;
 }
+
+generateRecords()
 
 let recordsWritten = 0;
 let writeRecords = () => {
@@ -72,6 +73,7 @@ let writeRecords = () => {
     let recordsToWrite = generateRecords();
     csvWriter.writeRecords(recordsToWrite)
     .then(() => {
+      console.log(`Good work! You have created ${recordsWritten} records!`)
       recordsWritten += recordsToWrite.length
       writeRecords()
     });
@@ -79,3 +81,5 @@ let writeRecords = () => {
     console.log(`SUCCESS! You generated ${variables.totalNumRecordsToWrite} records!!!`)
   }
 }
+
+writeRecords();
